c24af1df8aa9b6db29af8d4ef663c0c3095e771d
Tony Wu <bobacrypto2@gmail.com>
2022-06-26 23:44:25 -0700

    Add hover effects to squares and project is finish

--- script.js
+++ script.js
@@ -21,6 +21,29 @@ body.append(container, btnDiv);
 btnDiv.appendChild(create);
 
 
+// removes any children if they exist on the Etch and Sketch board
+function reset() {
+    let child = container.lastElementChild;
+
+    while(child) {
+        container.removeChild(child);
+        child = container.lastElementChild;
+    }
+}
+// changes each square to a random color when hovered over
+function hover() {
+    const child = container.querySelectorAll('div');
+
+    child.forEach(div => {
+        div.addEventListener('mouseover', (e) => {
+            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            e.target.style.backgroundColor = "#" + randomColor;
+        })
+    })
+}
+
+
+// prompt for an integer for an X by X grid and then creating said grid within the browser
 create.addEventListener('click', () => {
         let boardSize = prompt('Enter desired squares up to 100: ');
         let perimeter = 640 / boardSize;
@@ -31,16 +54,21 @@ create.addEventListener('click', () => {
             return; 
         }
 
+        if (container.children.length > 1) {
+            reset();
+        }
+
         if (!isNaN(boardSize) && boardSize <= 100 && boardSize > 0) {
             for (i = 0; i < area; i++) {
-                let square = document.createElement('div');
-                square.setAttribute('style', `width: ${perimeter}px; height: ${perimeter}px; background-color: plum; border: solid lime 1px; box-sizing: border-box;`);
+                const square = document.createElement('div');
+                square.setAttribute('style', `width: ${perimeter}px; height: ${perimeter}px; background-color: #FFFAF0; border: solid #696969 1px; box-sizing: border-box;`);
                 container.appendChild(square);
             }
+
+        hover();
+
         } else {
             alert(`${boardSize} is too high or not a positive integer!`);
             return;
         }
 });
-
-// Target divs within the Children of Container to set hover effects
